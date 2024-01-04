1f4965d9cf56f7d7f728befea81c8f14518c1031
Alex-Corzo <100803304+Alex-Corzo@users.noreply.github.com>
2024-01-03 18:40:48 -0600

    Tok tik Corzo

---
+++ flutter-toktik-main/README.md
@@ -0,0 +1,15 @@
+# Toktik - A Flutter Project
+## Importante:
+Los videos no están incluídos en el repositorio, debido a que son muy pesados y GitHub no lo permite.
+
+Renombrar los videos así, ya que es lo que se encuentra en el data source.
+```
+1.mp4
+2.mp4
+3.mp4
+4.mp4
+5.mp4
+6.mp4
+7.mp4
+8.mp4
+```
---
+++ flutter-toktik-main/lib/shared/data/local_video_posts.dart
@@ -0,0 +1,42 @@
+List<Map<String, dynamic>> videoPosts = [
+  {
+    'name': 'Feliz Año nuevo',
+    'videoUrl': 'assets/videos/1.mp4',
+    'likes': 20234,
+    'views': 1553245,
+  },
+  {
+    'name': 'Que no lo vea el jefe',
+    'videoUrl': 'assets/videos/2.mp4',
+    'likes': 14230,
+    'views': 30042,
+  },
+  {
+    'name': 'Se puso raro el TOKTIK',
+    'videoUrl': 'assets/videos/3.mp4',
+    'likes': 21354320,
+    'views': 163563,
+  },
+  {
+    'name': 'Ya dejen a mi carnal',
+    'videoUrl': 'assets/videos/4.mp4',
+    'likes': 4260,
+    'views': 12800,
+  },
+  {
+    'name': 'Para que se puedan dar una idea',
+    'videoUrl': 'assets/videos/5.mp4',
+    'likes': 34780,
+    'views': 55473,
+  },
+  {
+    'name': 'Y el muro de carne?',
+    'videoUrl': 'assets/videos/6.mp4',
+    'likes': 1660,
+    'views': 3370,
+  },
+
+
+];
+
+
